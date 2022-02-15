b76cb1ea22d3f1b574c9fde0eb1272be2a4d4a93
20170375 <62216628+20170375@users.noreply.github.com>
2022-02-15 22:58:16 +0900

    2nd day updated

--- popup.js
+++ popup.js
@@ -1,11 +1,18 @@
 'use strict';
 
 function bnt1_fn() {
+    // jquery code
+    chrome.tabs.executeScript(null, {
+        file: 'jquery.js',
+        allFrames: true,
+        runAt: "document_idle"
+    });
+
     // sum3에서 크롤링해서 텍스트박스에 보여주기
     chrome.tabs.executeScript(null, {
-        file: 'sum.js'
-    }, function() {
-        window.close();
+        file: 'sum.js',
+        allFrames: true,
+        runAt: "document_idle"
     });
 };
 
