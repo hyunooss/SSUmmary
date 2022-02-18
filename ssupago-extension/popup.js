07ad70ec4078e3997d6712ec0fc49cecf0a457b2
20170375 <62216628+20170375@users.noreply.github.com>
2022-02-18 20:02:27 +0900

    4th day updated

--- ssupago-extension/popup.js
+++ ssupago-extension/popup.js
@@ -2,17 +2,17 @@
 
 function bnt1_fn() {
     // jquery code
-    chrome.tabs.executeScript(null, {
-        file: 'jquery.js',
-        allFrames: true,
-        runAt: "document_idle"
+    chrome.tabs.executeScript({
+        file: 'jquery.js'
+    }, function(result) {
+        document.getElementById('result').innerText = "요약중...";
     });
 
     // sum3에서 크롤링해서 텍스트박스에 보여주기
-    chrome.tabs.executeScript(null, {
-        file: 'sum.js',
-        allFrames: true,
-        runAt: "document_idle"
+    chrome.tabs.executeScript({ 
+        file: 'sum.js', 
+    }, function (result) {
+        document.getElementById('result').innerText = result[0];
     });
 };
 
