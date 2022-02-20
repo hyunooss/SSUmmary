d132e235f5713c4fa7c41dfc711d9d0a3b853031
20170375 <62216628+20170375@users.noreply.github.com>
2022-02-20 18:37:16 +0900

    5th day updated

--- ssupago-extension/popup.js
+++ ssupago-extension/popup.js
@@ -8,15 +8,47 @@ function bnt1_fn() {
         document.getElementById('result').innerText = "요약중...";
     });
 
-    // sum3에서 크롤링해서 텍스트박스에 보여주기
+    // sum.js code
     chrome.tabs.executeScript({ 
         file: 'sum.js', 
     }, function (result) {
+        // print result
         document.getElementById('result').innerText = result[0];
+
+        // set result to storage
+        chrome.storage.sync.set({'result': result[0]});
+    });
+
+    chrome.tabs.executeScript({ 
+        code: "chrome.storage.sync.set({'url': document.location.href});"
     });
 };
 
+
+// when popup.html is loaded
 document.addEventListener('DOMContentLoaded', function () {   
     // add click event to button
     document.getElementById("btn1").addEventListener('click', bnt1_fn);
+
+    // get result from storage
+    chrome.storage.sync.get(function (data) {
+        let result = data.result ? data.result : "";
+
+        chrome.tabs.executeScript({ 
+            code: "chrome.storage.sync.get(function (data) { \
+                let url = data.url ? data.url : ''; \
+                return url; \
+            });"
+        });
+            
+        let textarea = document.getElementById('result')
+        textarea.innerText = data.url + "\n" +  document.location.href;
+
+        // if url is not same, then clear result
+        if (data.url !== document.location.href) {
+            result = "";
+        }
+
+        // document.getElementById('result').innerText = result;
+    });
 });
