a949d9cc76667fcf614de4e2948afad4a092d259
20170375 <62216628+20170375@users.noreply.github.com>
2022-02-23 19:04:49 +0900

    8th day updated

--- ssupago-extension/floating_button.js
+++ ssupago-extension/floating_button.js
@@ -1,43 +1,54 @@
 var button = document.createElement('div'); 
 button.id = 'floating-button';
 button.innerText = "Click";
-button.style.position = 'fixed';
-button.style.bottom = '20px';
-button.style.right = '20px';
-button.style.width = '40px';
-button.style.height = '40px';
-button.style.backgroundColor = '#00bcd4';
-button.style.textAlign = 'center';
-button.style.color = 'white';
-button.style.lineHeight = '40px';
-button.style.cursor = 'pointer';
-button.style.borderRadius = '10px';
-button.style.zIndex = '999';
+button.classList.add('small');
 document.body.appendChild(button);
 
+
+button.onmouseover = function(event) {
+    // add  big class
+    event.target.className = 'big';
+};
+
+button.onmouseleave = function(event) {
+    // add small class
+    event.target.className = 'small';
+};
+
+
 var iframe = document.createElement('iframe');
+iframe.src = chrome.runtime.getURL('popup.html');
+iframe.scrolling = 'no';
+
 
 button.onclick = function() {
     // floating popup iframe
-    iframe.src = chrome.runtime.getURL('popup.html');
-    iframe.style.position = 'fixed';
-    iframe.style.bottom = '40px';
-    iframe.style.right = '40px';
-    iframe.style.width = '250px';
-    iframe.style.height = '170px';
-    iframe.style.border = 'none';
-    iframe.scrolling = 'no';
-    iframe.style.zIndex = '9999';
-    iframe.style.backgroundColor = 'white';
-    iframe.style.opacity = '0.95';
-    iframe.style.borderRadius = '5px';
     document.body.appendChild(iframe);
 };
 
+
+// close when click out of iframe
 window.onclick = function(event) {
     if (event.target !== button) {
         if (document.body.hasChildNodes(iframe)) {
             document.body.removeChild(iframe);
         }
     }
+};
+
+
+// button draggable
+button.onmousedown = function(event) {
+    var x = event.clientX - button.offsetLeft;
+    var y = event.clientY - button.offsetTop;
+    var mousemove = function(event) {
+        button.style.left = event.clientX - x + 'px';
+        button.style.top = event.clientY - y + 'px';
+    }
+    var mouseup = function() {
+        document.removeEventListener('mousemove', mousemove);
+        document.removeEventListener('mouseup', mouseup);
+    }
+    document.addEventListener('mousemove', mousemove);
+    document.addEventListener('mouseup', mouseup);
 };
