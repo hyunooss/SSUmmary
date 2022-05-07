7c13f823e9c5c6252c35783c3423dd1637bfc351
20170375 <62216628+20170375@users.noreply.github.com>
2022-05-07 19:01:08 +0900

    Update views.py

--- django-server/ssummary_site/views.py
+++ django-server/ssummary_site/views.py
@@ -17,11 +17,18 @@ def ssummary(request):
         ## ---시간측정--- ##
         import time
         start = time.time()
+        # 번역
         text_kor = converter.translate(text, input_size=5000)
         print("\n번역 소요시간:", time.time() - start)
 
+        # 문법 검사
         start = time.time()
-        text_sum = converter.summarize(text_kor, deep=deep)
+        text_checked_kor = converter.spell_check(text_kor)
+        print("\n검사 소요시간:", time.time() - start)
+
+        start = time.time()
+        # 요약
+        text_sum = converter.summarize(text_checked_kor, deep=deep)
         if deep == True: print("깊은 ", end="")
         else: print("얕은 ", end="")
         print("요약 소요시간:", time.time() - start)
